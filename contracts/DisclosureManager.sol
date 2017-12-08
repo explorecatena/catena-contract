ea2a4e5a9712373eaef91b4f35342316abcbf3e5
Shayan Eskandari <shayanb@users.noreply.github.com>
2017-12-08 12:42:15 -0500

    Add rejector function to prevent payments

--- contracts/DisclosureManager.sol
+++ contracts/DisclosureManager.sol
@@ -157,4 +157,8 @@ contract DisclosureManager {
 		// No event for pullEntry() since it shouldn't cost gas to call it
 	}
 
+	//Prevent payments to the contract
+ 	 function () public payable {
+    	revert();
+  	}
 }
