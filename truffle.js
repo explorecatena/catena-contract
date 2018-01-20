810fbe2d0396086d51db7cb564b9065a068186f5
Dylan Seago <dylan.seago@gmail.com>
2018-01-19 19:39:23 -0500

    Remove live publishing config

--- truffle.js
+++ truffle.js
@@ -1,8 +1,3 @@
-const ProviderEngine = require('web3-provider-engine');
-const Web3Subprovider = require('web3-provider-engine/subproviders/web3.js');
-const DelegatedWalletSubprovider = require('./lib/delegated-wallet-subprovider.js');
-const { HttpProvider } = require('web3').providers;
-
 const GWEI = 1000000000;
 
 const defaults = {
@@ -18,29 +13,9 @@ const testrpc = {
   network_id: '*',
 };
 
-const liveDeployGasPrice = 2 * GWEI;
-const liveTxGasPrice = 0.5 * GWEI;
-
-const liveProvider = () => {
-  const engine = new ProviderEngine();
-
-  // Localhost for wallet only
-  engine.addProvider(new DelegatedWalletSubprovider(new Web3Subprovider(new HttpProvider('http://localhost:8645'))));
-
-  // Public node for the rest
-  engine.addProvider(new Web3Subprovider(new HttpProvider('https://web3.faa.st/eth')));
-
-  engine.on('error', console.error);
-
-  engine.start();
-  return engine;
-};
-
 const live = {
-  provider: liveProvider,
   network_id: '1',
-  gasPrice: liveDeployGasPrice, // Truffle only uses this for deploys
-  txGasPrice: liveTxGasPrice, // Custom prop for manual use in scripts
+  gasPrice: 2 * GWEI, // Truffle only uses this for deploys
 };
 
 const ropsten = {
