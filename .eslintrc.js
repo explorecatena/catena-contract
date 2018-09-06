03c67f5911d9f6579fd568607a106abb70704626
Dylan Seago <dylan.seago@gmail.com>
2018-09-06 18:24:18 -0400

    Disable comma-dangle eslint rule

--- .eslintrc.js
+++ .eslintrc.js
@@ -8,7 +8,8 @@ module.exports = {
     'arrow-parens': 0,
     'no-console': 0,
     'import/no-extraneous-dependencies': 0,
-    'class-methods-use-this': 0
+    'class-methods-use-this': 0,
+    'comma-dangle': 0
   },
   'globals': {
     'artifacts': true
